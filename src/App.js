cd9924a145bbe562a19a3f9ea8afe547b82704b4
Glitch (hello-sqlite) <none>
2020-10-06 21:15:32 +0000

    🏍🌜 Checkpoint ./src/App.js:17666850/22604 ./watch.json:17666850/2

--- src/App.js
+++ src/App.js
@@ -1,89 +1,65 @@
 import React from "react";
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 import "./App.css";
 
 class App extends React.Component {
   renderNavbar() {
     return (
-      <Navbar class="navbar navbar-expand-md navbar-light fixed-top bg-uw-gray-blue">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/en/thumb/e/e0/University_of_Wisconsin-Madison_logo.svg/1200px-University_of_Wisconsin-Madison_logo.svg.png"
-          height="50px"
-          width="auto"
-          class="navbar-brand border-round"
-        />
-        <a class="navbar-brand" href="#">
-          Coca-Corona
-        </a>
-        <Button
-          class="navbar-toggler collapsed"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarCollapse"
-          aria-controls="navbarCollapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </Button>
-        <div class="navbar-collapse collapse" id="navbarCollapse">
-          <ul class="navbar-nav mr-auto">
-            <li class="nav-item active align-self-center">
-              <a class="nav-link" href="#">
-                Home <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item align-self-center">
-              <a class="nav-link" href="#overview">
-                Overview
-              </a>
-            </li>
-            <li class="nav-item align-self-center">
-              <a class="nav-link" href="#accounts">
-                Accounts
-              </a>
-            </li>
-            <li class="nav-item align-self-center">
-              <a class="nav-link" href="#beneficiary">
-                Beneficiaries
-              </a>
-            </li>
-          </ul>
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item align-self-center">
-              <button
-                type="button"
-                onclick="logout()"
-                class="btn btn-primary bg-uw-red mt-2 mt-md-0"
-              >
-                Log out
-              </button>
-            </li>
-          </ul>
-        </div>
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">Coca-Corona</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <Form.Control
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+          <Button variant="primary">
+            Log out
+          </Button>
+        </Navbar.Collapse>
       </Navbar>
     );
   }
   render() {
     return (
       <>
-        <head>
+        <header>
           <link
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
             crossOrigin="anonymous"
           />
-        </head>
-
-        <header>{this.renderNavbar()}</header>
-        <body>
-          <main>
-            <h1>TEST!</h1>
-          </main>
-        </body>
+          {this.renderNavbar()}
+        </header>
+        <main>
+          <h1>TEST!</h1>
+        </main>
       </>
     );
   }
