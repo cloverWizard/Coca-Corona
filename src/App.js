da8381245beed4da55576f9ef97e509fc7118af7
Glitch (hello-sqlite) <none>
2020-10-06 21:27:29 +0000

    💓🐄 Checkpoint ./src/App.js:17666850/9300

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from "react";
 
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import NavDropdown from "react-bootstrap/NavDropdown";
-import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 
 import "./App.css";
@@ -12,36 +10,16 @@ class App extends React.Component {
   renderNavbar() {
     return (
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">Coca-Corona</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href="#dashboard">Dashboard</Nav.Link>
+            <Nav.Link href="#map">Map</Nav.Link>
+            <Nav.Link href="#contacts">Contacts</Nav.Link>
+            <Button variant="primary">Log out</Button>
           </Nav>
-          <Form inline>
-            <Form.Control
-              type="text"
-              placeholder="Search"
-              className="mr-sm-2"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          <Button variant="primary">
-            Log out
-          </Button>
         </Navbar.Collapse>
+        <Navbar.Brand href="#home">Coca-Corona</Navbar.Brand>
       </Navbar>
     );
   }
@@ -55,6 +33,18 @@ class App extends React.Component {
             integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
             crossOrigin="anonymous"
           />
+          <script
+            type="module"
+            src="https://unpkg.com/@myuw-web-components/myuw-profile@latest?module"
+          ></script>
+          <script
+            noModule
+            src="https://unpkg.com/@myuw-web-components/myuw-profile@latest"
+          ></script>
+          <script>
+            customElements .whenDefined('myuw-profile') .then(() =>
+            document.dispatchEvent(new CustomEvent('myuw-login', {}))) ;
+          </script>
           {this.renderNavbar()}
         </header>
         <main>
