5c2a5cdaf76783d274074136ba370907c88f3f58
Glitch (hello-sqlite) <none>
2020-10-06 20:52:34 +0000

    🍗🐽 Checkpoint ./src/App.js:17666850/30677 ./src/Nav-Bar/Nav-Bar.js:17666850/485 ./src/NavBar/NavBar.js:17666850/820

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
 import "./App.css";
 
 class App extends React.Component {
-
+  
+  renderNavbar() {
+    return {
+      <>
+        </>
+    };
+  }
   render() {
     return (
       <>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossOrigin="anonymous"
-        />
-        
-        <p>TEST!</p>
+        <head>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossOrigin="anonymous"
+          />
+        </head>
 
+        <header>
+
+        </header>
+        <body>
+          <main>
+            <p>TEST!</p>
+          </main>
+        </body>
       </>
     );
   }
