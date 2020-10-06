b63f88af42fc8e001aacd213d02b68973e604c97
Glitch (hello-sqlite) <none>
2020-10-06 18:54:52 +0000

    🐁👆 Checkpoint ./src/App.js:17666850/720

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import React from "react";
 import "./App.css";
-import Tabs from "react-bootstrap/Tabs";
-import Tab from "react-bootstrap/Tab";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
 
   render() {
     return (
@@ -18,24 +12,9 @@ class App extends React.Component {
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossOrigin="anonymous"
         />
-
-        <Tabs
-          defaultActiveKey="search"
-          style={{
-            position: "fixed",
-            zIndex: 1,
-            width: "100%",
-            backgroundColor: "white"
-          }}
-        >
-          <Tab eventKey="search" title="Search" style={{ paddingTop: "5vh" }}>
-            <div style={{ marginLeft: "20vw" }}></div>
-          </Tab>
-
-          <Tab eventKey="cart" title="Cart" style={{ paddingTop: "5vh" }}>
-            <div style={{ marginLeft: "5vw" }}></div>
-          </Tab>
-        </Tabs>
+        
+        <p>TEST!</p>
+        <p>123123123</p>
       </>
     );
   }
